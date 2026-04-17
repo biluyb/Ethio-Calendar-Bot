909695845aa0c4aafe5b09bdeba4ac69401fee4a
biluyb <biluyb338@gmail.com>
2026-04-17 18:02:55 +0300

    Fix ImportError: move handle_admin_dm_send to correct module

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -9,12 +9,13 @@ from .user import (
 from .api import api_key_command, api_stats_command, api_stats_callback
 from .admin import (
     users, users_callback, groups_command, groups_callback, 
-    broadcast_command, send_msg_command, add_admin, del_admin, list_admins,
+    broadcast_command, send_msg_command, handle_admin_dm_send,
+    add_admin, del_admin, list_admins,
     block_command, unblock_command, leavegroup_command
 )
 from .callbacks import age_mode_callback, contact_admin_callback
 from .main_handler import (
     handle, admin_reply_callback, handle_admin_reply_to_user, 
-    unknown_command, handle_admin_dm_send
+    unknown_command
 )
 from .extra import help_command, health_url
