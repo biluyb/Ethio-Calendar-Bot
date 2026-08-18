c21160ae72ea5c216253d740ccd1a37a15799ac3
biluyb <biluyb338@gmail.com>
2026-08-18 10:43:35 +0300

    Todays output display changed

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -167,15 +167,16 @@ async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # Build requested format:
         # 🇺🇸 08 - 07 - 2026 | Wednesday, July - 08
         # 🇪🇹 1 - 11 - 2018 | ረቡዕ - ሐምሌ - 1
-        greg_formatted = f"🇺🇸{g_day:02d} - {g_month:02d} - {g_year} | {g_day_name}, {g_month_name},{g_day:02d}"
+        greg_formatted = f"🇺🇸{g_day:02d} - {g_month:02d} - {g_year} |{g_day_name} {g_month_name} {g_day:02d}"
         
         if lang == "am":
-            eth_formatted = f"🇪🇹{e_day} - {e_month} - {e_year} | {e_day_name}, {e_month_name}, {e_day}"
+            eth_formatted = f"🇪🇹{e_day} - {e_month} - {e_year} | {e_day_name} {e_month_name} {e_day}"
             eva_txt = f" (ዘመነ {evangelist['am']})" if evangelist else ""
             msg = (
                 f"📅 <b>የዛሬ ቀን መረጃ (Today)</b>\n\n"
+                f"<code>{eth_formatted}</code>\n"
                 f"<code>{greg_formatted}</code>\n"
-                f"<code>{eth_formatted}</code>{eva_txt}\n"
+                f"<b>{eva_txt}</b>\n"
                 f"━━━━━━━━━━━━━━━━━\n"
             )
             if hol:
@@ -187,7 +188,7 @@ async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
             from app.handlers.calendar_view import ETH_MONTHS_EN
             # e_month_name_en = ETH_MONTHS_EN[e_month - 1]
             # e_day_name_en = EN_DAYS[now.weekday()]
-            eth_formatted = f"🇪🇹{e_day}-{e_month}-{e_year} | {e_day_name}-{e_month_name}-{e_day}"
+            eth_formatted = f"🇪🇹{e_day}- {e_month}- {e_year} | {e_day_name} {e_month_name} {e_day}"
             eva_txt = f"Year of {evangelist['en']}" if evangelist else ""
             msg = (
                 f"📅 <b>Today's Date Info</b>\n\n"
